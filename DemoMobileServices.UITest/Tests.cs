26fee0115b714731c8a6dcea4e040ef4efb3a2b4
Jesus Angulo <jesus.angulo@outlook.com>
2017-01-27 21:54:18 -0500

    Adding ui test

---
+++ DemoMobileServices.UITest/Tests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace DemoMobileServices.UITest
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.WaitForElement(a=> a.Id("button"));
+            app.Screenshot("First screen.");
+        }
+    }
+}
+
